53a293b16c554f7fe2b27b489396914dcfad87b4
Luís Soares <luis.soares@springernature.com>
2020-12-22 11:19:46 +0000

    Use sequences in Day 2

--- Day2.kts
+++ Day2.kts
@@ -4,14 +4,14 @@ import java.nio.file.Paths
 import java.util.*
 
 // part 1
-val testInput = listOf(
+val sampleInput = listOf(
     Policy(1, 3, 'a') to "abcde",
     Policy(1, 3, 'b') to "cdefg",
     Policy(2, 9, 'c') to "ccccccccc"
 )
-check(2 == checkValidity(testInput, ::validate1))
+check(2 == checkValidity(sampleInput.asSequence(), ::validate1))
 
-fun checkValidity(input: List<Pair<Policy, String>>, validator: (Pair<Policy, String>) -> Boolean) =
+fun checkValidity(input: Sequence<Pair<Policy, String>>, validator: (Pair<Policy, String>) -> Boolean) =
     input.count { validator(it) }
 
 fun validate1(row: Pair<Policy, String>) =
@@ -19,8 +19,8 @@ fun validate1(row: Pair<Policy, String>) =
 
 data class Policy(val int1: Int, val int2: Int, val char: Char)
 
-val path = "${Paths.get("").toAbsolutePath()}/input/2.txt"
-val input = Scanner(FileInputStream(File(path))).useDelimiter("\n").asSequence()
+fun loadFile() = Scanner(FileInputStream(File("${Paths.get("").toAbsolutePath()}/input/2.txt")))
+    .useDelimiter("\n").asSequence()
     .map {
         val row = it.split(" ")
         Policy(
@@ -29,14 +29,13 @@ val input = Scanner(FileInputStream(File(path))).useDelimiter("\n").asSequence()
             char = row[1].first()
         ) to row.last()
     }
-    .toList()
 
-println(checkValidity(input, ::validate1))
+println(checkValidity(loadFile(), ::validate1))
 
 // part 2
-check(1 == checkValidity(testInput, ::validate2))
+check(1 == checkValidity(sampleInput.asSequence(), ::validate2))
 
 fun validate2(row: Pair<Policy, String>) =
     (row.second[row.first.int1 - 1] == row.first.char) xor (row.second[row.first.int2 - 1] == row.first.char)
 
-println(checkValidity(input, ::validate2))
+println(checkValidity(loadFile(), ::validate2))
