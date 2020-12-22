f0e3f7ee5174d791839838eb2eef94c180e9c095
Luís Soares <luis.soares@springernature.com>
2020-12-22 23:24:02 +0000

    Simplify

--- Day5.kts
+++ Day5.kts
@@ -1,6 +1,6 @@
 import java.io.File
 import java.io.FileInputStream
-import java.lang.Math.pow
+import java.lang.Integer.parseInt
 import java.nio.file.Paths
 import java.util.*
 
@@ -11,22 +11,14 @@ check(820 == findHighestSeatId(sampleInput))
 
 fun findHighestSeatId(input: Sequence<String>) = input.map(::convert).maxOrNull()
 
-fun convert(input: String) = convertPart(input.dropLast(3)) * 8 + convertPart(input.drop(7))
+fun convert(boardingSpec: String) = convertPart(boardingSpec.dropLast(3)) * 8 + convertPart(boardingSpec.drop(7))
 
-fun convertPart(input: String) = input
-    .foldIndexed(0 to pow(2.0, input.length - 1.0)) { index, acc, c ->
-        val weight = pow(2.0, input.length - 1.0 - index).toInt()
-        when (c) {
-            'F', 'L' -> acc.first to (acc.second - weight)
-            'B', 'R' -> (acc.first + weight) to acc.second
-            else -> error("can't process")
-        }
-    }.first
+fun convertPart(boardingPass: String) = boardingPass
+    .replace(Regex("[FL]"), "0")
+    .replace(Regex("[BR]"), "1")
+    .let { parseInt(it, 2) }
 
 fun loadFile() = Scanner(FileInputStream(File("${Paths.get("").toAbsolutePath()}/input/5.txt")))
     .asSequence()
 
 println(findHighestSeatId(loadFile()))
-
-// part 2
-
