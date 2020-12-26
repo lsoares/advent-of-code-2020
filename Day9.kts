62ef5bfc25929b6c5ae7f14d516177b22fc24977
Luís Soares <luis.soares@springernature.com>
2020-12-26 14:13:13 +0000

    solve day 9 part 2

--- Day9.kts
+++ Day9.kts
@@ -6,23 +6,46 @@ import java.util.*
 // --- Part One ---
 val sampleInput = listOf<Long>(35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182, 127, 219, 299, 277, 309, 576)
 
-fun findWrong(preambleSize: Int, input: List<Long>): Long {
+fun findWrongIndex(preambleSize: Int, input: List<Long>, index: Int = preambleSize): Int {
     val valid = input.take(preambleSize).nChoose2().map { it.first + it.second }.distinct()
-    val current = input.drop(preambleSize).first()
+    val current = input.drop(preambleSize).firstOrNull() ?: error("not found")
     return if (valid.contains(current))
-        findWrong(preambleSize, input.drop(1))
-    else current
+        findWrongIndex(preambleSize, input.drop(1), index + 1)
+    else index
 }
 
 fun <T> List<T>.nChoose2() =
     flatMap { i -> mapNotNull { j -> i.takeIf { it != j }?.let { i to j } } }
 
-check(127L == findWrong(5, sampleInput))
+println(findWrongIndex(5, sampleInput))
+check(127L == sampleInput[findWrongIndex(5, sampleInput)])
 
 val path = "${Paths.get("").toAbsolutePath()}/input/9.txt"
 val input = Scanner(FileInputStream(File(path))).useDelimiter("\n").asSequence()
     .map(String::toLong).toList()
 
-println(findWrong(25, input)) // 15690279
+println(input[findWrongIndex(25, input)]) // 15690279
 
+// --- Part Two ---
+fun findContiguousWrong(preambleSize: Int, input: List<Long>): Long {
+    val invalidValue = input[findWrongIndex(preambleSize, input)]
 
+    for (i in input.indices) {
+        var sum = 0L
+        var j = i
+        do {
+            sum += input[j]
+            if (sum == invalidValue && j != i) {
+                return input.subList(i, j).let {
+                    it.minOrNull()!! + it.maxOrNull()!!
+                }
+            }
+            j++
+        } while (sum < invalidValue)
+    }
+    error("not found")
+}
+
+check(62L == findContiguousWrong(5, sampleInput))
+
+println(findContiguousWrong(25, input)) // 2174232
