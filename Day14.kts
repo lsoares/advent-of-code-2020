00add3c17f509917468fd75706a24730f21c1cbe
Luís Soares <lsoares@gmail.com>
2021-01-05 09:50:20 +0000

    WiP

--- Day14.kts
+++ Day14.kts
@@ -3,11 +3,12 @@ import Day14.Program.Instruction.Type.SET_MASK
 import Day14.Program.Instruction.Type.SET_VALUE
 import java.io.File
 import java.io.FileInputStream
+import java.lang.Math.pow
 import java.nio.file.Paths
 import java.util.*
 
 // --- Part One ---
-val sampleInput = sequenceOf(
+val sampleInput1 = sequenceOf(
     "mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X",
     "mem[8] = 11",
     "mem[7] = 101",
@@ -27,7 +28,7 @@ fun parse(instructionStr: String) = instructionStr.let {
     )
 }
 
-check(165L == process(sampleInput).sumAll())
+check(165L == process(sampleInput1).sumAll())
 
 data class Program(val instructions: Sequence<Instruction>) {
     data class Instruction(val type: Type, val arg0: String, val arg1: String? = null) {
@@ -39,7 +40,7 @@ data class BitMask(val mask: String = "X".repeat(36)) {
     private val andMask = mask.replace("X", "1").toLong(2)
     private val orMask = mask.replace("X", "0").toLong(2)
 
-    fun applyTo(number: Long) = number and andMask or orMask
+    fun applyTo(value: Long) = value and andMask or orMask
 }
 
 data class Memory(private val mem: Map<Int, Long> = emptyMap(), val bitMask: BitMask = BitMask()) {
@@ -62,3 +63,57 @@ val input = Scanner(FileInputStream(File(path))).useDelimiter(System.lineSeparat
     .map(::parse)
 
 check(12610010960049L == process(input).sumAll())
+
+// --- Part Two ---
+
+data class Memory2(private val mem: Map<Long, Long> = emptyMap(), val bitMask: BitMask2 = BitMask2()) {
+    fun set(address: Long, value: Long) =
+        copy(mem = bitMask.applyTo(address).fold(mem) { memEach, addressEach ->
+            memEach + (addressEach to value)
+        })
+
+    fun sumAll() = mem.values.sum()
+}
+
+data class BitMask2(val mask: String = "0".repeat(36)) {
+    private val orMask = mask.replace("X", "0").toLong(2)
+
+    private val combinations by {
+        val maxComb = pow(2.0, mask.count { it == 'X' }.toDouble()).toInt()
+        (0 until maxComb).map {
+            it.toString(2).padStart(mask.count { it == 'X' }, '0')
+        }.map(::applyCombination)
+    }
+
+    private fun applyCombination(string: String): String {
+        val m = mask.toMutableList()
+        var curr = 0
+        m.forEachIndexed { index: Int, c: Char ->
+            if (c == 'X') {
+                m[index] = string[curr]
+                curr++
+            }
+        }
+        return m.joinToString("")
+    }
+
+    fun applyTo(address: Long): List<Long> = combinations.
+//    listOf(address or orMask)
+}
+
+fun process2(instructions: Sequence<Instruction>) = instructions
+    .fold(Memory2()) { memory, (type, arg0, arg1) ->
+        when (type) {
+            SET_MASK -> memory.copy(bitMask = BitMask2(arg0))
+            SET_VALUE -> memory.set(arg0.toLong(), arg1?.toLong() ?: error("missing arg"))
+        }
+    }
+
+val sampleInput2 = sequenceOf(
+//    "mask = 000000000000000000000000000000X1001X",
+    //"mem[42] = 100",
+    "mask = 00000000000000000000000000000000X0XX",
+    //"mem[26] = 1",
+).map(::parse)
+
+println(process2(sampleInput2))
